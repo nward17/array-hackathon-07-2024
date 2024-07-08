accbc9a1b64c2bea0cfc1b21a8f0c1a37af3d3f4
nward17 <nicolas.ward17@gmail.com>
2024-07-08 15:12:18 -0700

    Adding armor stand cleanup command

--- minecraft-fabric/src/main/java/github/papi/PAPI.java
+++ minecraft-fabric/src/main/java/github/papi/PAPI.java
@@ -3,8 +3,14 @@ package github.papi;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import github.papi.http.HTTP;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import org.slf4j.Logger;
@@ -54,6 +60,19 @@ public class PAPI implements ModInitializer {
 
 		// Register a tick event to update armor stand positions
 		ServerTickEvents.END_SERVER_TICK.register(server -> piiManager.updatePlayerPIIPosition());
+
+		// Add a cleanup armor stands command
+		CommandRegistrationCallback.EVENT.register((dispatcher, dedicated, environment) -> {
+			dispatcher.register(CommandManager.literal("removeAllArmorStands")
+					.executes(context -> {
+						ServerCommandSource source = context.getSource();
+						ServerWorld world = source.getWorld();
+						for (ArmorStandEntity armorStand : world.getEntitiesByType(EntityType.ARMOR_STAND, entity -> true)) {
+							armorStand.remove(Entity.RemovalReason.DISCARDED);
+						}
+						return 1;
+					}));
+		});
     }
 
 	private PII convertJsonToPlayerPII(String jsonResponse) throws Exception {
